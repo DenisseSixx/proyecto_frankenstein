3df64f0219585bf5cf9584cd253eb73877e72213
Denisse Estefania Lugo Arenivas <denisse.lugo.mh@gmail.com>
2023-10-17 11:36:55 -0600

    predrawer

---
+++ RickandMorty/rickandmorty/lib/screens/home_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rickandmorty/providers/rick_provider.dart';
+import 'package:rickandmorty/widgets/widgets.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final rickProvider = Provider.of<RickProvider>(context);
+    
+    return Scaffold(
+     
+ appBar: AppBar(
+
+     flexibleSpace: Image(
+          image: AssetImage('assets/BAR.png'),
+          fit: BoxFit.fitHeight) ,        
+      
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search_outlined),
+          )
+        ],
+        title: const Center(
+        //  child: Text('Personajes'),
+        ),
+      ),
+      
+      body: Column(
+        
+
+        children: [ 
+          
+          Image.asset('assets/bar.jpg'),
+          
+       CharacterSlider(
+            characters: rickProvider.characters),
+             Image.asset('assets/rmorty.gif'),
+          ],
+      ),
+    );
+  }
+}
